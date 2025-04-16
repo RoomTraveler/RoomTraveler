e75f226f8c602ff8f864ebe1d678e084efce3647
dj258255 <dj258255@naver.com>
2025-04-16 23:33:50 +0900

    기본적인 JPA 게시판 CURD 완성
    검색은 QueryDSL 이용

--- RoomTraveler/src/main/java/org/roomtraveler/roomtrip/controller/BoardController.java
+++ RoomTraveler/src/main/java/org/roomtraveler/roomtrip/controller/BoardController.java
@@ -1,5 +1,6 @@
 package org.roomtraveler.roomtrip.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.roomtraveler.roomtrip.dto.BoardDTO;
@@ -8,8 +9,11 @@ import org.roomtraveler.roomtrip.dto.PageResponseDTO;
 import org.roomtraveler.roomtrip.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -28,5 +32,80 @@ public class BoardController {
 
         model.addAttribute("responseDTO", responseDTO);
 
+    }
+
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        log.info("board POST register...");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors");
+            redirectAttributes.addFlashAttribute("errors",bindingResult.getAllErrors() );
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result",bno);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+    }
+
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO,
+                         @Valid BoardDTO boardDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes){
+        log.info("게시판 수정 post..." + boardDTO);
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors");
+
+            String link = pageRequestDTO.getLink();
+
+            redirectAttributes.addFlashAttribute("errors",bindingResult.getAllErrors() );
+
+
+            redirectAttributes.addAttribute("bno",boardDTO.getBno());
+
+            return "redirect:/board/modify?" + link;
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modified");
+
+        redirectAttributes.addAttribute("bno",boardDTO.getBno());
+
+        return "redirect:/board/read";
+    }
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes){
+
+        log.info("게시글 삭제" + bno);
+
+        boardService.delete(bno);
+
+        redirectAttributes.addFlashAttribute("result", "removed");
+
+        return "redirect:/board/read";
     }
 }
